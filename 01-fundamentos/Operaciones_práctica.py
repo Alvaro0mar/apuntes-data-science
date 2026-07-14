147078d4d717d509d8c9e4c26b304aa8ed3566a6
Alvaro Omar Chillihua Zubieta <alvaro.99.c.z@gmail.com>
2026-07-14 13:20:31 -0500

    mensaje

--- 01-fundamentos/Operaciones_práctica.py
+++ 01-fundamentos/Operaciones_práctica.py
@@ -1,12 +1,12 @@
+import numpy as np
+import pandas as pd
+
 a = 1000
 print(a)
 b = 8
 print(b)
 c = a + b
 print(c)
-
-import numpy as np
-import pandas as pd
 
 arreglo = np.array([1, 2, 3, 4, 5])
 print(arreglo)
